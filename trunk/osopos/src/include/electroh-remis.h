36211e1e8c304769812dfa86bb724a2eec2ac765
linucs <linucs@8f325012-0c4c-4806-b633-2705aa58a59f>
2001-01-15 17:10:48 +0000

    Sample files of an already working environment

--- trunk/osopos/src/include/electroh-remis.h
+++ trunk/osopos/src/include/electroh-remis.h
@@ -1,11 +1,22 @@
 #include <stdio.h>
+#include <string.h>
 
+#ifndef ESC
 #define ESC 27
+#endif
 
-void imprime_razon_social(FILE *disp)
+void imprime_razon_social(FILE *disp, char *miniprinter_type)
 {
-  fprintf(disp,"%c%c%c              Electro Hogar\n",
-              ESC,'P',14);
-  fprintf(disp,"%c%c%c    Francisco Javier Mondragon Villa\n",
-                15,ESC,'M');
+  if (strstr(miniprinter_type, "EPSON")) {
+    //    fprintf(disp, "%ca%c%c!%cElectro Hogar\n", ESC, 1, ESC, 48);
+    fprintf(disp, "%ca%c%c!%cElectro Hogar\n%c!%c", ESC, 1, ESC, 48, ESC, 0);
+    fprintf(disp, "Francisco Javier Mondragón Villa\n%c!%c%ca%c", ESC, 1, ESC, 0);
+  }
+  else {
+    fprintf(disp,"%c%c%c    Electro Hogar\n",
+	    ESC,'P',14);
+    fprintf(disp,"%c%c%c  Francisco Javier Mondrag%cn Villa\n",
+	    15,ESC,'M', 186);
+  }
+
 }
