46851e2ece79e7492366288970ad73e32e902981
linucs <linucs@8f325012-0c4c-4806-b633-2705aa58a59f>
2001-07-09 18:39:30 +0000

    Actualizando con mi repositorio.

--- trunk/osopos/src/include/rchapa-remis.h
+++ trunk/osopos/src/include/rchapa-remis.h
@@ -1,11 +1,21 @@
 #include <stdio.h>
+#include <string.h>
 
+#ifndef ESC
 #define ESC 27
+#endif
 
-void imprime_razon_social(FILE *disp)
+void imprime_razon_social(FILE *disp, char *miniprinter_type)
 {
-  fprintf(disp,"%c%c%c     La Reina      \n",
-              ESC,'P',14);
-  fprintf(disp,"%c%c%c       Ricardo A. Chapa C%crdenas   \n",
-                15,ESC,'M', 206);
+  if (strstr(miniprinter_type, "EPSON")) {
+    fprintf(disp, "%ca%c%c!%cLa Reina\n%c!%c", ESC, 1, ESC, 48, ESC, 0);
+    fprintf(disp, "Ricardo Alejandro Chapa Ramos\n%c!%c%ca%c", ESC, 1, ESC, 0);
+  }
+  else {
+    fprintf(disp,"%c%c%c         La Reina \n",
+	    ESC,'P',14);
+    fprintf(disp,"%c%c%c   Ricardo Alejandro Chapa Ramos\n",
+	    15,ESC,'M');
+  }
+
 }
