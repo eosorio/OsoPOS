abca303fd4dab08e83534a81b820242420475058
linucs <linucs@8f325012-0c4c-4806-b633-2705aa58a59f>
2002-01-19 07:42:28 +0000

    Functions for printing using the ESC/P language and the IBM printer language for dot-matrix

---
+++ osopos/src/include/print-func.h
@@ -0,0 +1,108 @@
+/*   -*- mode: c; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ print-func.h 0.1-1 Biblioteca de funciones de impresión para OsoPOS.
+        Copyright (C) 1999,2000 Eduardo Israel Osorio Hernández
+
+        Este programa es un software libre; puede usted redistribuirlo y/o
+modificarlo de acuerdo con los términos de la Licencia Pública General GNU
+publicada por la Free Software Foundation: ya sea en la versión 2 de la
+Licencia, o (a su elección) en una versión posterior.
+
+        Este programa es distribuido con la esperanza de que sea útil, pero 
+SIN GARANTIA ALGUNA; incluso sin la garantía implícita de COMERCIABILIDAD o
+DE ADECUACION A UN PROPOSITO PARTICULAR. Véase la Licencia Pública General
+GNU para mayores detalles.
+
+        Debería usted haber recibido una copia de la Licencia Pública General
+GNU junto con este programa; de no ser así, escriba a Free Software
+Foundation, Inc., 675 Mass Ave, Cambridge, MA02139, USA.
+ */
+
+#include<stdio.h>
+
+unsigned Espacios(FILE* arch, unsigned nespacios);
+/* Imprime espacios en al archivo ar */
+
+void avance_vert(FILE *disp, unsigned avance, unsigned mode);
+/* Avanza n/216" en modo Epson */
+
+void select_pitch(FILE *disp, short unsigned pitch, unsigned mode);
+/* Escoge tamaño de letra */
+
+void select_charset(FILE *disp, short unsigned charset, unsigned mode);
+/* Escoge tipo de caracteres internacionales */
+
+void feed_paper(FILE *disp, short n, unsigned mode);
+/* Avanza n líneas de papel, donde n[0..127] */
+
+
+/*********************************************************************/
+
+unsigned Espacios (FILE* ar, unsigned n) {
+
+  unsigned i;
+
+  for (i=0; i<n; ++i)
+    fprintf(ar, " ");
+  return(i);
+}
+
+/*********************************************************************/
+
+void avance_vert(FILE *disp, unsigned avance, unsigned mode)
+{
+  if (mode==0 || mode==1)
+    fprintf(disp, "%c%c%c", ESC, 'J', avance);
+}
+
+/*********************************************************************/
+
+void select_pitch(FILE *disp, short unsigned pitch, unsigned mode)
+{
+
+  fprintf(disp, "%c", 18);
+
+  if (pitch>12)
+    fprintf(disp, "%c", 15);
+
+  if (mode == 0) { /* Epson */
+    switch(pitch) {
+      case 10:
+      case 17:
+        fprintf(disp, "%cP", ESC);
+      break;
+      default:
+        fprintf(disp ,"%cM",ESC);
+    }
+  }
+  else if(mode==1) { /* IBM */
+    switch(pitch) {
+      case 10:
+      case 17:
+        fprintf(disp, "%c", 18);
+        break;
+        default:
+      fprintf(disp, "%c%c", ESC, 58);
+    }
+  }
+}
+
+/*********************************************************************/
+
+void select_charset(FILE *disp, short unsigned chset, unsigned mode)
+{
+  if (mode == 0) {
+    fprintf(disp, "%cR%c", ESC, chset);
+  }
+}
+
+/*********************************************************************/
+
+void feed_paper(FILE *disp, short lines, unsigned mode)
+{
+  int i;
+
+  if (mode ==0)
+    fprintf(disp, "%c%c%c%c", 27,102,1,lines);
+  else
+    for (i=0; i<lines; i++, fprintf(disp, "\r"));
+}
