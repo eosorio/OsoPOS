807cd41a1d5f65dc23b3496c7360ce2d06a57438
linucs <linucs@8f325012-0c4c-4806-b633-2705aa58a59f>
2002-01-19 07:32:28 +0000

    Adding a flexible configuration, towards a generic user-friendly program.

--- trunk/osopos/src/imprrem.c
+++ trunk/osopos/src/imprrem.c
@@ -1,8 +1,8 @@
 /*   -*- mode: c; indent-tabs-mode: nil; c-basic-offset: 2 -*-
 
    OsoPOS Sistema auxiliar en punto de venta para pequeños negocios
-   Programa ImprRem 0.15 (C) 1999-2001 E. Israel Osorio H.
-   desarrollo@punto-deventa.com
+   Programa ImprRem 0.16 (C) 1999-2001 E. Israel Osorio H.
+   desarrollo@elpuntodeventa.com
    Lea el archivo README, COPYING y LEAME que contienen información
    sobre la licencia de uso de este programa
 
@@ -26,13 +26,12 @@ Foundation, Inc., 675 Mass Ave, Cambridge, MA02139, USA.
 #include <time.h> 
 #include "include/pos-func.h" 
 
-/*#define maxitem 5*/	/* Número máximo de artículos diferentes en */
-			/* remision */
-#define vers "0.15-1"
+#define _IMPRREM
+#define vers "0.16-2"
 
 #define maxrenglon 8
 #define maxdes 39
-#define maxbuf 130
+#define mxbuff 130
 #define ESC 27
 #define FF 12
 #define SI 15
@@ -45,28 +44,34 @@ Foundation, Inc., 675 Mass Ave, Cambridge, MA02139, USA.
 #define FALSE 0
 #endif
 
-char buff1[maxbuf],buff2[maxbuf];
+#include "include/minegocio-remis.h"
+
+char buff1[mxbuff],buff2[mxbuff];
 struct articulos art[maxart];
-char nmimpre[maxbuf],
-  tipoimp[maxbuf],	/* Tipo de impresora */
-  lp_printer[maxbuf];
+char nmimpre[mxbuff];
+char *lp_printer, *prt_type; /* Tipo de impresora */
 int numarts, dgaran=-1;
 short unsigned maxitem;
 short iva_incluido;
 short desglose_iva;
 short unsigned ajuste_microv, ajuste_v, ajuste_h;
+short unsigned price_pos[3];
+unsigned total_price_pos, desc_pos, desc_len, qt_pos, code_pos;
+short unsigned date_pos;
+short date_pitch = 12;
+short article_pitch = 17;
 char *home_directory;
 
 int read_config();
 int CalculaIVA();
-void ImprimeRemision(int numart);
-int imprime(int numart);
+
 
 int read_config() {
   char       *nmconfig;
   FILE       *config;
-  char       buff[maxbuf],buf[maxbuf];
+  char       buff[mxbuff],buf[mxbuff];
   char       *b;
+  char       *aux = NULL;
   static int i;
 
 
@@ -83,13 +88,19 @@ int read_config() {
   iva_incluido = 1;
   desglose_iva = 0;
   strcpy(nmimpre,"/tmp/osopos_nota");
-  strcpy(tipoimp,"EPSON");
-  strncpy(lp_printer, "facturas", maxbuf);
+  prt_type = calloc(1, strlen("EPSON"));
+  strcpy(prt_type,"EPSON");
+  lp_printer = calloc(1, strlen("facturas")+1);
+  strcpy(lp_printer, "facturas");
   maxitem = 20;
 
   ajuste_v = 0;
   ajuste_microv = 0;
   ajuste_h = 0;
+
+  price_pos[0] = 30;
+  price_pos[1] = 40;
+  price_pos[2] = 0;
 
   config = fopen(nmconfig,"r");
   if (config) {		/* Si existe archivo de configuración */
@@ -114,14 +125,25 @@ int read_config() {
         strcpy(nmimpre,buf);
       }
       else if (!strcmp(b,"impresora.lpr")) {
-        strncpy(buf, strtok(NULL,"="), maxbuf);
-        strncpy(lp_printer,buf, maxbuf);
+        strncpy(buf, strtok(NULL,"="), mxbuff);
+        aux = realloc(lp_printer, strlen(buf)+1);
+        if (aux != NULL)
+          strcpy(lp_printer,buf);
+        else
+          fprintf(stderr,"imprrem. Error de memoria en argumento de configuracion %s\n",
+                  b);
       }
       else if (!strcmp(b,"impresora.tipo")) {
         strcpy(buf, strtok(NULL,"="));
-        strcpy(tipoimp,buf);
-        for (i=0; i<strlen(tipoimp); i++)
-          tipoimp[i] = toupper(tipoimp[i]);
+        aux = realloc(prt_type, strlen(buf)+1);
+        if (aux != NULL) {
+          strcpy(prt_type,buf);
+          for (i=0; i<strlen(prt_type); i++)
+            prt_type[i] = toupper(prt_type[i]);
+        }
+        else
+          fprintf(stderr,"imprrem. Error de memoria en argumento de configuracion %s\n",
+                  b);
       }
       else if (!strcmp(b,"iva.incluido")) {
         strcpy(buf, strtok(NULL,"="));
@@ -132,21 +154,66 @@ int read_config() {
         desglose_iva = (buf[0] == 'S');
       }
       else if (!strcmp(b,"maxitem")) {
-	strcpy(buf, strtok(NULL,"="));
-	maxitem = (unsigned) atoi(buf);
+        strcpy(buf, strtok(NULL,"="));
+        maxitem = (unsigned) atoi(buf);
       }
       else if (!strcmp(b,"ajuste.microv")) {
-	strcpy(buf, strtok(NULL,"="));
-	ajuste_microv = (unsigned) atoi(buf);
+        strcpy(buf, strtok(NULL,"="));
+        ajuste_microv = (unsigned) atoi(buf);
       }
       else if (!strcmp(b,"ajuste.v")) {
-	strcpy(buf, strtok(NULL,"="));
-	ajuste_v = (unsigned) atoi(buf);
+        strcpy(buf, strtok(NULL,"="));
+        ajuste_v = (unsigned) atoi(buf);
       }
       else if (!strcmp(b,"ajuste.h")) {
-	strcpy(buf, strtok(NULL,"="));
-	ajuste_h = (unsigned) atoi(buf);
+        strcpy(buf, strtok(NULL,"="));
+        ajuste_h = (unsigned) atoi(buf);
       }
+      else if (!strcmp(b,"importe.1")) {
+        strcpy(buf, strtok(NULL,"="));
+        price_pos[0] = (unsigned) atoi(buf);
+      }
+      else if (!strcmp(b,"importe.2")) {
+        strcpy(buf, strtok(NULL,"="));
+        price_pos[1] = (unsigned) atoi(buf);
+      }
+      else if (!strcmp(b, "importe.3")) {
+        strcpy(buf, strtok(NULL, "="));
+        price_pos[2] = (unsigned) atoi(buf);
+      }
+      else if (!strcmp(b,"importe.total")) {
+        strcpy(buf, strtok(NULL,"="));
+        total_price_pos = (unsigned) atoi(buf);
+      }
+      else if (!strcmp(b,"fecha.pos_v")) {
+        strcpy(buf, strtok(NULL,"="));
+        date_pos = (unsigned) atoi(buf);
+      }
+      else if (!strcmp(b,"fecha.tamaño")) {
+        strcpy(buf, strtok(NULL,"="));
+        date_pitch = (unsigned) atoi(buf);
+      }
+      else if (!strcmp(b,"codigo.pos")) {
+        strcpy(buf, strtok(NULL,"="));
+        code_pos = (unsigned) atoi(buf);
+      }
+      else if (!strcmp(b,"cantidad.pos")) {
+        strcpy(buf, strtok(NULL,"="));
+        qt_pos = (unsigned) atoi(buf);
+      }
+      else if (!strcmp(b,"descripcion.pos")) {
+        strcpy(buf, strtok(NULL,"="));
+        desc_pos = (unsigned) atoi(buf);
+      }
+      else if (!strcmp(b,"descripcion.long")) {
+        strcpy(buf, strtok(NULL,"="));
+        desc_len = (unsigned) atoi(buf);
+      }
+      else if (!strcmp(b,"articulos.tamaño")) {
+        strcpy(buf, strtok(NULL,"="));
+        article_pitch = (unsigned) atoi(buf);
+      }
+
       fgets(buff,sizeof(buff),config);
     }
     fclose(config);
@@ -170,192 +237,6 @@ int CalculaIVA() {
   return(i);
 }
 
-void avance_vert(FILE *disp, unsigned avance)
-{
-  fprintf(disp, "%c%c%c", ESC, 'J', avance);
-
-}
-
-void ImprimeRemision(int numart) {
-  FILE *impresora;
-  static int i=0, renglon;
-  static int ib;
-  time_t tiempo;
-  struct tm *fecha;
-
-  impresora = fopen(nmimpre,"w");
-  if (!impresora) {
-    fprintf(stderr,
-	"ERROR: No se puede escribir al archivo %s\n",nmimpre);
-    fprintf(stderr,"Quiza no se cuenta con los permisos necesarios\n");
-    exit(10);
-  }
-  fprintf(impresora,"%c",18);
-  /* Cancela modo condensado */
-  tiempo = time(NULL);
-  fecha = localtime(&tiempo);
-  fecha->tm_year += 1900;
-
-       /* Imprime encabezado */
-  fprintf(impresora,"\n\n\n\n\n\n");
-  fprintf(impresora,"%3d   %3d  %5d",fecha->tm_mday,
-          fecha->tm_mon +1 ,fecha->tm_year);
-  fprintf(impresora,"\r\n\n");
-  fprintf(impresora,"%c%c%cM",ESC,15,ESC); /* 17cpi */
-  fprintf(impresora,"\n\n\n\n\n%c%c%c",ESC,'J',890);
-  /* 890/216" de avance vertical */
-
-  /* Imprime articulos */
-  for (i=0,renglon=0; i<numart; i++,renglon++) {
-    fprintf(impresora,"%3d     %-39s    %8.2f\n\r",
-	art[i].cant,art[i].desc,art[i].cant*art[i].pu);
-    if (renglon<maxitem)
-      avance_vert(impresora,15); /* 15/216" entre renglones */
-  }
-
-  /* Imprime renglones vacios faltantes */
-  for (; renglon<maxitem; renglon++) {
-     /* 2 renglones por el Subtotal y el IVA, */
-    fprintf(impresora,"\n\r");
-    if (renglon<maxitem)
-      avance_vert(impresora,15); /* 15/216" entre renglones */
-  }
-
-     /* Imprime subtotal, IVA y total */
-  avance_vert(impresora,3); /* 3/216" de avance */
-
-  if (dgaran < 0)
-    sprintf(buff1,"(     dias de garantia)");
-  else
-    sprintf(buff1,"( %3d dias de garantia)",dgaran);
-
-  fprintf(impresora, "        %-42s",buff1);
-
-  if (desglose_iva) {
-    fprintf(impresora, "%8.2f\n\r", subtotal);
-    fprintf(impresora, "%49s %8.2f\n\r", "", iva);
-  }
-  else
-    fprintf(impresora, "\n\n\r");
-
-  avance_vert(impresora,15); /* 15/216" entre renglones */
-  fprintf(impresora,"     %45s%8.2f\r\n","",total);
-  avance_vert(impresora,15);
-
-  strncpy(buff1,str_cant(total, &ib),sizeof(buff1));
-
-  /* Importe con letra */
-  fprintf(impresora,"%16s%spesos\r\n%16s"," ",buff1," ");
-  if (ib>9)
-    fprintf(impresora,"%2d/100 M.N.\r\n",ib);
-  else	/* Asegura que los centavos se impriman a dos digitos */
-    fprintf(impresora,"0%1d/100 M.N.\r\n",ib);
-
-  fprintf(impresora,"%c",FF);
-  fclose(impresora);
-}
-
-
-int imprime(int numart) {
-  FILE *impresora;
-  static int i=0, renglon;
-  static int ib;
-  time_t tiempo;
-  struct tm *fecha;
-  char str_buff[maxbuf];
-
-  impresora = fopen(nmimpre,"w");
-  if (!impresora) {
-    fprintf(stderr,
-	"ERROR: No se puede escribir al archivo %s\n",nmimpre);
-    fprintf(stderr,"Quiza no se cuenta con los permisos necesarios\n");
-    exit(10);
-  }
-  fprintf(impresora,"%c",18);
-  /* Cancela modo condensado */
-  tiempo = time(NULL);
-  fecha = localtime(&tiempo);
-  fecha->tm_year += 1900;
-
-  for (i=0; i<ajuste_v; i++)
-    fprintf(impresora, "\n");
-
-  avance_vert(impresora, ajuste_microv);
-
-   /* Imprime encabezado */
-  fprintf(impresora,"\n\n");
-  Espacios(impresora, ajuste_h);
-  fprintf(impresora,"%2d  %3d  %4d",fecha->tm_mday,
-          fecha->tm_mon +1 ,fecha->tm_year);
-  fprintf(impresora,"\r\n\n");
-  fprintf(impresora,"%c%c%cM",ESC,15,ESC); /* 17cpi */
-  fprintf(impresora,"\n\n\n\n\n%c%c%c",ESC,'J',890);
-  /* 890/216" de avance vertical */
-
-  /* Imprime articulos */
-  for (i=0,renglon=0; i<numart; i++,renglon++) {
-    Espacios(impresora, ajuste_h);
-    fprintf(impresora,"%2d     %-39s    %8.2f\n\r",
-	art[i].cant,art[i].desc,art[i].cant*art[i].pu);
-    if (renglon<maxitem)
-      avance_vert(impresora,15); /* 15/216" entre renglones */
-  }
-
-  /* Imprime renglones vacios faltantes */
-  for (; renglon<maxitem+(desglose_iva*2); renglon++) {
-     /* 2 renglones por el Subtotal y el IVA, */
-    fprintf(impresora,"\n\r");
-    if (renglon<maxitem)
-      avance_vert(impresora,15); /* 15/216" entre renglones */
-  }
-
-     /* Imprime subtotal, IVA y total */
-  avance_vert(impresora,3); /* 3/216" de avance */
-
-  if (dgaran < 0)
-    sprintf(buff1,"(     dias de garantia)");
-  else
-    sprintf(buff1,"( %3d dias de garantia)",dgaran);
-
-  Espacios(impresora, ajuste_h);
-  fprintf(impresora, "      %-46s",buff1);
-
-  if (desglose_iva) {
-    fprintf(impresora, "%6.2f\n\r", subtotal);
-    Espacios(impresora, ajuste_h);
-    fprintf(impresora, "%46s %6.2f\n\r", "", iva);
-    avance_vert(impresora, 20); /* 20/216" entre renglones */
-    Espacios(impresora, 51);
-  }
-
-  fprintf(impresora,"%6.2f\r\n", total);
-  avance_vert(impresora,15);
-
-  strncpy(buff1,str_cant(total, &ib),sizeof(buff1));
-
-  /* Importe con letra */
-  Espacios(impresora, ajuste_h);
-  Espacios(impresora, 15);
-  fprintf(impresora,"%spesos\r\n", buff1);
-  Espacios(impresora, ajuste_h);
-  Espacios(impresora, 15);
-  if (ib>9)
-    fprintf(impresora,"%2d/100 M.N.\r\n",ib);
-  else	/* Asegura que los centavos se impriman a dos digitos */
-    fprintf(impresora,"0%1d/100 M.N.\r\n",ib);
-
-  fprintf(impresora,"%c",FF);
-  fclose(impresora);
-  sprintf(str_buff, "lpr -Fb -P %s %s", lp_printer, nmimpre);
-  impresora = popen(str_buff, "w");
-  i = pclose(impresora);
-  sprintf(str_buff, "rm -rf %s", nmimpre);
-  impresora = popen (str_buff, "r");
-  pclose(impresora);
-  return(i);
-}
-
-
 int main(int argc, char *argv[])
 {
   unsigned num_venta = 0;
@@ -377,8 +258,7 @@ int main(int argc, char *argv[])
 
   if (numarts>0) {
     CalculaIVA();
-    //    ImprimeRemision(numarts);
-    imprime(numarts);
+    imprime(nmimpre, art, numarts);
     PQfinish(conn);
     return (OK);
   }
