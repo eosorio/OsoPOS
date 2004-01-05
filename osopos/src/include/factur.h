7db0c2d4bfd25c5c5d072710e323f25d9c3b11ff
linucs <linucs@8f325012-0c4c-4806-b633-2705aa58a59f>
2004-01-05 10:19:50 +0000

    Agrego nuevas configuraciones

--- osopos/src/include/factur.h
+++ osopos/src/include/factur.h
@@ -28,20 +28,34 @@ FILE *basefact;
 struct datoscliente cliente;
 struct fech fecha;
 struct articulos art[maxart];
-char buffer;
+unsigned almacen;
+struct db_data db;
 struct tm *f;
 /*time_t tiempo;*/
 
-char /*nmimpre[mxbuff], */
-      nmfact[mxbuff],
-      tipoimp[mxbuff],
-      lprimp[mxbuff],
-      puerto_imp[mxbuff];
+char  *lprimp,
+  *tipoimp,
+  *nmfact,
+  *nmfact,
+  *disp_lector_serie;  /* Ruta al scanner de c. de barras serial */
+char   s_divisa[3];       /* Designación de la divisa que se usa para cobrar en la base de datos */
+double TAX_PERC_DEF; /* Porcentaje de IVA por omisión */
 char *home_directory;
+char *log_name;
+
+int lector_serial; /* Cierto si se usa un scanner serial */
+int serial_crlf=1; /* 1 si el scanner envia CRLF, 0 si solamente LF */
+tcflag_t serial_bps=B38400; /* bps por omisión */
+int wait_flag=TRUE;                    /* TRUE while no signal received */
+char s_buf[255];    /* Buffer de puerto serie */
+struct termios oldtio,newtio;  /* Parametros anteriores y nuevos de terminal serie */
+struct sigaction saio;              /* definition of signal action */
+int fd;            /* Descriptor de archivo de puerto serie */
 
 unsigned maxrenf;
 unsigned numarticulos;
-short    impresion_directa;
+short unsigned maxitemr;
+int iva_incluido;
 
 int  read_config();
 int  RegistraFactura(unsigned numfact, struct articulos art[maxarts], PGconn *conexion);
@@ -50,96 +64,340 @@ int  BuscaCliente(char *rfc, struct datoscliente *clien, PGconn *con);
 
 /************************************************************************/
 
+int init_config()
+{
+  FILE *env_process;
+
+  home_directory = calloc(1, 255);
+  log_name = calloc(1, 255);
+
+  if (!(env_process = popen("printenv HOME", "r"))) {
+    free(log_name);
+    free(home_directory);
+    return(PROCESS_ERROR);
+  }
+  fgets(home_directory, 255, env_process);
+  home_directory[strlen(home_directory)-1] = 0;
+  pclose(env_process);
+
+  if (!(env_process = popen("printenv LOGNAME", "r"))) {
+    free(log_name);
+    free(home_directory);
+    return(PROCESS_ERROR);
+  }
+  fgets(log_name, 255, env_process);
+  log_name[strlen(log_name)-1] = 0;
+  pclose(env_process);
+
+  lprimp = calloc(1, strlen("facturas")+20);
+  strcpy(lprimp, "facturas");
+
+  disp_lector_serie = calloc(1, strlen("/dev/barcode")+1);
+  strcpy(disp_lector_serie, "/dev/barcode");
+  lector_serial = 0;
+
+  tipoimp = calloc(1, strlen("EPSON")+20);
+  strcpy(tipoimp, "EPSON");
+ 
+  db.name= NULL;
+  db.user = NULL;
+  db.passwd = NULL;
+  db.sup_user = NULL;
+  db.sup_passwd = NULL;
+  db.hostport = NULL;
+  db.hostname = NULL;
+
+  db.hostname = calloc(1, strlen("255.255.255.255"));
+  db.name = calloc(1, strlen("elpuntodeventa.com"));
+  db.user = calloc(1, strlen(log_name)+1);
+  strcpy(db.user, log_name);
+  db.sup_user = calloc(1, strlen("scaja")+1);
+
+  maxitemr = 6;
+
+  iva_incluido = 0;
+  TAX_PERC_DEF = 15;
+  strcpy(s_divisa, "MXP");
+
+  almacen = 1;
+
+  nmfact = calloc(1, strlen("/tmp/factura")+1);
+  strcpy(nmfact,"/tmp/factura");
+  iva_porcentaje = 10;
+  maxrenf = 20;
+
+  return(OK);
+}
+
+/************************************************************************/
+
 int read_config() {
 
   char *nmconfig;
   FILE *config;
-  char *buf;   /* Cadena modificable para busqueda de tokens */
-  char *valor;
-  char *linea;
-  int i;
+  char buff[mxbuff],buf[mxbuff];
+  char *b;
+  char *aux = NULL;
+  //  struct kbd_struct teclado;
 
-  home_directory = calloc(1, 255);
   nmconfig = calloc(1, 255);
-  config = popen("printenv HOME", "r");
-  fgets(home_directory, 255, config);
-  home_directory[strlen(home_directory)-1] = 0;
-  pclose(config);
-
-  strcpy(nmconfig, home_directory);
+  strncpy(nmconfig, home_directory, 255);
   strcat(nmconfig, "/.osopos/factur.config");
-
-  strcpy(nmfact,"/tmp/factura");
-  strcpy(tipoimp,"EPSON");
-  strcpy(lprimp, "facturas");
-  iva_porcentaje = 10;
-  impresion_directa = 0;
-  strcpy(puerto_imp, "/dev/lp");
-  maxrenf = 20;
 
   config = fopen(nmconfig,"r");
   if (config != NULL) {         /* Si existe archivo de configuración */
-    buf = calloc(1, mxbuff);
-    valor = calloc(1, mxbuff);
-    linea = calloc(1, mxbuff);
-    //    *b = calloc(1, mxbuff);
-    //    buf = *b;
-
-    fgets(linea,mxbuff,config);
+    b = buff;
+    fgets(buff,mxbuff,config);
     while (!feof(config)) {
-      linea [ strlen(linea) - 1 ] = 0;
-
-      if (!strlen(linea) || linea[0] == '#') { /* Linea vacía o coment. */
-        fgets(linea,mxbuff,config);
+      buff [ strlen(buff) - 1 ] = 0;
+      if (!strlen(buff) || buff[0] == '#') { /* Linea vacía o coment. */
+        if (!feof(config))
+          fgets(buff,mxbuff,config);
         continue;
       }
-
-      strcpy(buf, linea);  /* Variable modificable */
-      strcpy(valor, strtok(buf,"="));
+      strncpy(buf, strtok(buff,"="), mxbuff);
 
         /* Busca parámetros de impresora y archivo de última venta */
-      if (!strcmp(valor, "factura")) {
-        strcpy(nmfact, strtok(NULL,"="));
+      if (!strcmp(b, "factura")) {
+        strncpy(buf, strtok(NULL,"="), mxbuff);
+        aux = realloc(nmfact, strlen(buf)+1);
+        if (aux != NULL) {
+          strcpy(nmfact, buf);
+          aux = NULL;
+        }
+        else
+          fprintf(stderr, "factur. Error de memoria en argumento de configuracion %s\n", b);
       }
-      /*      else if (!strcmp(valor,"registro")) {
+      /*      else if (!strcmp(b,"registro")) {
         strcpy(nmlog, strtok(NULL,"="));
         }*/
-      else if (!strcmp(valor,"renglones.articulos")) {
-        maxrenf = atoi(strtok(NULL,"="));
+      else if (!strcmp(b,"renglones.articulos")) {
+        strncpy(buf, strtok(NULL,"="), mxbuff);
+        maxrenf = atoi(buf);
       }
-      else if (!strcmp(valor,"iva.porcentaje")) {
+      else if (!strcmp(b,"iva.porcentaje")) {
         iva_porcentaje = atof(strtok(NULL,"="));
       }
-      else if (!strcmp(valor,"impresora.tipo")) {
-        strcpy(tipoimp, strtok(NULL,"="));
-        for (i=0; i<strlen(tipoimp); i++)
-          tipoimp[i] = toupper(tipoimp[i]);
+      else if (!strcmp(b,"impresora.tipo")) {
+        strncpy(buf, strtok(NULL,"="), mxbuff);
+        aux = realloc(tipoimp, strlen(buf)+1);
+        if (aux != NULL) {
+          strcpy(tipoimp, buf);
+          aux = NULL;
+        }
+        else
+          fprintf(stderr,
+                  "factur. Error de memoria en argumento de configuracion %s\n",
+                  b);
       }
-      else if (!strcmp(valor,"impresion.directa")) {
-        strcpy(linea, strtok(NULL, "="));
-        impresion_directa = toupper(linea[0])=='S';
-      }
-      else if (!strcmp(valor,"impresora.lpr")) {
+      else if (!strcmp(b,"impresora.lpr")) {
         strcpy(lprimp, strtok(NULL,"="));
       }
-      else if (!strcmp(valor,"impresora.puerto")) {
-        strcpy(puerto_imp, strtok(NULL,"="));
-      }
-
-      fgets(linea,mxbuff,config);
+      if (!feof(config))
+        fgets(buff,mxbuff,config);
     }
     fclose(config);
-    free(valor);
-    free(buf);
-    free(linea);
+    if (aux != NULL)
+      aux = NULL;
     free(nmconfig);
     return(0);
   }
   free(nmconfig);
+  b = NULL;
   return(1);
 }
 
 /******************************************************************/
+
+int read_global_config()
+{
+  char *nmconfig;
+  FILE *config;
+  char buff[mxbuff],buf[mxbuff];
+  char *b;
+  char *aux = NULL;
+  int i_buf;
+  
+  nmconfig = calloc(1, 255);
+  strncpy(nmconfig, "/etc/osopos/remision.config", 255);
+
+  config = fopen(nmconfig,"r");
+  if (config) {         /* Si existe archivo de configuración */
+    b = buff;
+    fgets(buff,mxbuff,config);
+    while (!feof(config)) {
+      buff [ strlen(buff) - 1 ] = 0;
+      if (!strlen(buff) || buff[0] == '#') {
+        if (!feof(config))
+          fgets(buff,mxbuff,config);
+        continue;
+      }
+      strncpy(buf, strtok(buff,"="), mxbuff);
+        /* La función strtok modifica el contenido de la cadena buff    */
+        /* remplazando con NULL el argumento divisor (en este caso "=") */
+        /* por lo que b queda apuntando al primer token                 */
+
+      if (!strcmp(b, "impresora.lpr")) {
+        strncpy(buf, strtok(NULL,"="), mxbuff);
+        aux = realloc(lprimp, strlen(buf)+1);
+        if (aux != NULL) {
+          strcpy(lprimp, buf);
+          aux = NULL;
+        }
+        else
+          fprintf(stderr,
+                  "factur. Error de memoria en argumento de configuracion %s\n",
+                  b);
+       }
+      else if (!strcmp(b,"impresora.tipo")) {
+        strncpy(buf, strtok(NULL,"="), mxbuff);
+        aux = realloc(tipoimp, strlen(buf)+1);
+        if (aux != NULL) {
+          strcpy(tipoimp, buf);
+          aux = NULL;
+        }
+        else
+          fprintf(stderr,
+                  "factur. Error de memoria en argumento de configuracion %s\n",
+                  b);
+      }
+      else if (!strcmp(b,"db.host")) {
+        strncpy(buf, strtok(NULL,"="), mxbuff);
+        aux = realloc(db.hostname, strlen(buf)+1);
+        if (aux != NULL) {
+          strcpy(db.hostname,buf);
+          aux = NULL;
+        }
+        else
+          fprintf(stderr, "factur. Error de memoria en argumento de configuracion %s\n",
+                  b);
+      }
+      else if (!strcmp(b,"db.port")) {
+        strncpy(buf, strtok(NULL,"="), mxbuff);
+        aux = realloc(db.hostport, strlen(buf)+1);
+        if (aux != NULL) {
+          strcpy(db.hostport,buf);
+          aux = NULL;
+        }
+        else
+          fprintf(stderr, "factur. Error de memoria en argumento de configuracion %s\n",
+                  b);
+      }
+      else if (!strcmp(b,"db.nombre")) {
+        strncpy(buf, strtok(NULL,"="), mxbuff);
+        aux = realloc(db.name, strlen(buf)+1);
+        if (aux != NULL) {
+          strcpy(db.name,buf);
+          aux = NULL;
+        }
+        else
+          fprintf(stderr, "factur. Error de memoria en argumento de configuracion %s\n",
+                  b);
+      }
+      /*      else if (!strcmp(b,"db.usuario")) {
+        strncpy(buf, strtok(NULL,"="), mxbuff);
+        aux = realloc(db.user, strlen(buf)+1);
+        if (aux != NULL) {
+          strcpy(db.user,buf);
+          aux = NULL;
+        }
+        else
+          fprintf(stderr, "factur. Error de memoria en argumento de configuracion %s\n",
+                  b);
+                  }*/
+      else if (!strcmp(b,"db.sup_usuario")) {
+        strncpy(buf, strtok(NULL,"="), mxbuff);
+        aux = realloc(db.sup_user, strlen(buf)+1);
+        if (aux != NULL) {
+          strcpy(db.sup_user,buf);
+          aux = NULL;
+        }
+        else
+          fprintf(stderr, "factur. Error de memoria en argumento de configuración %s\n",
+                  b);
+      }
+      else if (!strcmp(b,"db.sup_passwd")) {
+        strncpy(buf, strtok(NULL,"="), mxbuff);
+        db.sup_passwd = calloc(1, strlen(buf)+1);
+        if (db.sup_passwd  != NULL) {
+          strcpy(db.sup_passwd,buf);
+          aux = NULL;
+        }
+        else
+          fprintf(stderr, "factur. Error de memoria en argumento de configuracion %s\n",
+                  b);
+      }
+      else if (!strcmp(b,"renglones.articulos")) {
+        strncpy(buf, strtok(NULL,"="), mxbuff);
+        maxitemr = atoi(buf);
+      }
+      else if (!strcmp(b,"porcentaje_iva")) {
+        strncpy(buf, strtok(NULL,"="), mxbuff);
+        TAX_PERC_DEF = atoi(buf);
+      }
+     else if (!strcmp(b,"almacen.numero")) {
+        strncpy(buf, strtok(NULL,"="), mxbuff);
+        almacen = atoi(buf);
+      }
+      else if(!strcmp(b, "scanner_serie")) {
+        lector_serial = 1;
+        strncpy(buf, strtok(NULL,"="), mxbuff);
+        aux = realloc(disp_lector_serie, strlen(buf)+1);
+        if (aux != NULL) {
+          strcpy(disp_lector_serie, buf);
+          aux = NULL;
+        }
+        else
+          fprintf(stderr, "factur. Error de memoria en argumento de configuracion %s\n",
+                  b);
+      }
+      else if(!strcmp(b, "scanner_velocidad")) {
+        strncpy(buf, strtok(NULL,"="), mxbuff);
+        i_buf = atoi(buf);
+        switch (i_buf) {
+        case 2400:
+          serial_bps = B2400;
+          break;
+        case 4800:
+          serial_bps = B4800;
+          break;
+        case 9600:
+          serial_bps = B9600;
+          break;
+        case 19200:
+          serial_bps = B19200;
+          case 38400:
+        default:
+          serial_bps = B38400;
+        }
+      }
+      else if(!strcmp(b, "divisa")) {
+        strncpy(buf, strtok(NULL,"="), MX_LON_DIVISA);
+        strcpy(s_divisa, buf);
+      }
+     else if (!strcmp(b,"iva_incluido")) {
+       strncpy(buf, strtok(NULL,"="), mxbuff);
+       iva_incluido = atoi(buf);
+     }
+     if (!feof(config))
+       fgets(buff,mxbuff,config);
+    }
+    fclose(config);
+    if (aux != NULL)
+      aux = NULL;
+    free(nmconfig);
+    b = NULL;
+    return(0);
+  }
+  if (aux != NULL)
+    aux = NULL;
+  free(nmconfig);
+  b = NULL;
+  return(1);
+}
+
+/***************************************************************************/
+
 
 int BuscaCliente(char *rfc, struct datoscliente *cliente, PGconn *con) {
   PGresult *resultado;
@@ -279,7 +537,10 @@ int CalculaIVA() {
 
   iva = 0;
   for (i=0; i<numarticulos; i++) {
-    mult_iva = art[i].iva_porc / 100;
+    if (art[i].iva_porc > 0)
+      mult_iva = art[i].iva_porc / 100;
+    else
+      mult_iva = 0;
     art[i].pu =  art[i].pu / (mult_iva+1);
     sumatoria += (art[i].pu * art[i].cant);
     iva += art[i].pu * mult_iva * art[i].cant;
