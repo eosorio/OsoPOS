a472c072cd62958803431c89f7f25f23d85a8b63
linucs <linucs@8f325012-0c4c-4806-b633-2705aa58a59f>
2009-04-03 21:46:10 +0000

    Cambio sistema de encriptaciÃ³n DES por SHA1. La base de datos debe ser cambiada para que el campo de password ahora pueda almacenar al menos 64 caracteres. Ideal: cambiar a varchar(512)

--- osopos/src/include/pos-bd-func.h
+++ osopos/src/include/pos-bd-func.h
@@ -1,9 +1,15 @@
+#include <openssl/sha.h>
+
 int verif_passwd(PGconn *con, gchar *login, gchar *passwd);
 
 int verif_passwd(PGconn *con, gchar *login, gchar *passwd) {
   gchar *query, *us_passwd;
-  char *enc_passwd;
+  unsigned char hash[20];
+  char hashed_passwd[41];
+  char hex_char[3];
   PGresult *db_r;
+  SHA_CTX ctx; 
+  int i=0;
 
   query = g_strdup_printf("SELECT passwd FROM users WHERE \"user\"='%s'", login);
   db_r = PQexec(con, query);
@@ -15,10 +21,22 @@ int verif_passwd(PGconn *con, gchar *login, gchar *passwd) {
   if (PQntuples(db_r) == 0)
 	return(FALSE);
 
-  us_passwd = g_strdup(PQgetvalue(db_r, 0, 0));
-  enc_passwd = crypt(passwd, us_passwd);
+  SHA1_Init(&ctx);
+  SHA1_Update(&ctx, passwd, strlen(passwd));
+  SHA1_Final(hash, &ctx);
 
-  if (strcmp(enc_passwd, us_passwd))
+  hashed_passwd[0] = 0;
+  for (i=0; i<20; i++) {
+    sprintf(hex_char, "%02x", hash[i]);
+    hashed_passwd[i*2] = hex_char[0];
+    hashed_passwd[i*2+1] = hex_char[1];
+  }
+  hashed_passwd[40] = 0;
+
+  us_passwd = g_strdup(PQgetvalue(db_r, 0, 0));
+  //  enc_passwd = crypt(passwd, "$1$ch8wq$");
+
+  if (strcmp(hashed_passwd, us_passwd))
 	return(FALSE);
   else
 	return(TRUE);
